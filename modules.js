caa16172b09354ef03b45f0a510dc784a727591d
Ebuka Nwannunu <47655083+ebuka1999@users.noreply.github.com>
2022-01-13 12:52:55 +0000

    firs commit

---
+++ modules.js
@@ -0,0 +1,9 @@
+const names = require('./names')
+const sayHi = require('./utils')
+const data = require('./alternative-export')
+
+require('./mind-grenade')
+
+sayHi('ebuka')
+sayHi(names.john)
+sayHi(names.peter)
